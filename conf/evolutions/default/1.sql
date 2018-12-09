994b9bd69f9a02e09dfd4760d10bce99eae00be2
projectormato <k.keita811.anuy@gmail.com>
2018-12-09 13:15:37 +0900

    fix db schema.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 create table todo (
   id                        bigint not null auto_increment,
   name                      varchar(255) not null,
-  todo_type                 bigint not null
+  todo_type                 bigint not null,
   constraint pk_todo primary key (id))
 ;
 create sequence todo_seq start with 2;
