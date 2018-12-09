89e2f8f0a09d0ec2611fc0f6fc7f708e91ca5b58
projectormato <k.keita811.anuy@gmail.com>
2018-12-09 11:43:46 +0900

    fix schema.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,12 +4,12 @@
 create table todo (
   id                        bigint not null auto_increment,
   name                      varchar(255) not null,
-  type                      int not null
+  todo_type                 int not null
   constraint pk_todo primary key (id))
 ;
 create sequence todo_seq start with 2;
 
-insert into todo (id,name) values (1,'ToDo', 1);
+insert into todo (id,name,todo_type) values (1,'ToDo', 1);
 
 # --- !Downs
 drop table if exists todo;
