2968620b963ad12b62c6043e6d6e6ede84cd8516
projectormato <k.keita811.anuy@gmail.com>
2018-12-31 12:44:06 +0900

    fix heroku postgresql settings.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,19 +2,14 @@
 
 # --- !Ups
 create table todo (
---   id                        bigint not null auto_increment,
   id                        serial,
   name                      varchar(255) not null,
   todo_type                 bigint not null,
   primary key (id)
---   constraint pk_todo primary key (id)
-  );
--- create sequence todo_seq start with 2;
+);
 
--- insert into todo (id,name,todo_type) values (1,'ToDo', 1);
 insert into todo (name,todo_type) values ('ToDo', 1);
 
 # --- !Downs
 drop table if exists todo;
 
--- drop sequence if exists todo_seq;
